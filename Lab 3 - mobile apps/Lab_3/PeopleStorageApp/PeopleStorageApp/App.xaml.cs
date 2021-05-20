1266d3e6033db1ec31ff0660a72782ddc4dd6dd9
rastarad <63683606+rastarad@users.noreply.github.com>
2021-05-20 12:42:53 +0200

    Add Lab 3 - mobile apps

---
+++ Lab 3 - mobile apps/Lab_3/PeopleStorageApp/PeopleStorageApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PeopleStorageApp.DataContracts;
+using Xamarin.Forms;
+
+namespace PeopleStorageApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var client = RestEase.RestClient.For<IPeopleClient>("http://192.168.1.4:6001/api");
+
+            InitializeComponent();
+
+            MainPage = new MainPage(client);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
